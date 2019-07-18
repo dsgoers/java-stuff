d634dbb645e08a1ac67230bb35468c195772825b
dsgoers <daniel.goers@cru.org>
2019-07-18 11:59:47 -0400

    Add LineBreakToCommaConverter

---
+++ src/LineBreakToCommaConverter.java
@@ -0,0 +1,35 @@
+public class LineBreakToCommaConverter {
+    private static final String lineBreakDelimitedList = "Java\n" +
+            "J2EE\n" +
+            "Maven\n" +
+            "Wildfly AS\n" +
+            "Hibernate ORM\n" +
+            "JPA\n" +
+            "REST\n" +
+            "SQL\n" +
+            "PostgreSQL\n" +
+            "MySQL\n" +
+            "Ruby\n" +
+            "Rails\n" +
+            "JavaScript\n" +
+            "React Native\n" +
+            "GraphQL\n" +
+            "Apollo GraphQL client\n" +
+            "JSON:API\n" +
+            "Travis CI\n" +
+            "ECS\n" +
+            "S3\n" +
+            "SNS\n" +
+            "GitHub\n" +
+            "Jenkins\n" +
+            "IntelliJ\n" +
+            "RubyMine";
+
+    private String execute() {
+        return lineBreakDelimitedList.replace("\n", ", ");
+    }
+
+    public static void main(String [] args) {
+        System.out.println(new LineBreakToCommaConverter().execute());
+    }
+}
